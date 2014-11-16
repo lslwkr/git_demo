9b926ee888b1ff796f7575eb114c5c138d295ecc
Jerry Lin <jerr.lin@sap.com>
2014-11-16 11:57:05 +0800

    delete todo and add something in master br

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         // add something in MB1 br
+        // delete TODO and add something in master br
 	}
 
 }
