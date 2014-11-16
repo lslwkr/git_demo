8801e6287f9d5caa295e492b85dffbd055dca23d
Jerry Lin <jerr.lin@sap.com>
2014-11-16 12:00:08 +0800

    add something in MB1 br

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,6 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
         // delete TODO and add something in master br
+		// TODO Auto-generated method stub
+        // add something in MB1 br
 	}
 
 }
